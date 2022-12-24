1571b0d2a628997ed4eddeed877ee840e4dad738
belkisarslan <belkisarslan33@gmail.com>
2022-12-24 12:56:04 +0300

    counter style

--- src/counter/Counter.js
+++ src/counter/Counter.js
@@ -1,24 +1,29 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { decrement, increment } from './counterSlice'
+import '../index.css';
 
 export function Counter() {
   const count = useSelector((state) => state.counter.value)
   const dispatch = useDispatch()
 
   return (
-    <div>
-      <div>
-      <button
+    <div className='main'>
+      <div className='container'>
+      <button className='btn'
           aria-label="Decrement value"
           onClick={() => dispatch(decrement())}>
-          Azalt
+          <span class="material-symbols-outlined">
+          remove
+          </span>
         </button>
-        <span>{count}</span>
-        <button
+        <span className='span'>{count}</span>
+        <button className='btn'
           aria-label="Increment value"
           onClick={() => dispatch(increment())}>
-          Arttır
+          <span class="material-symbols-outlined">
+          add
+          </span>
         </button>
         
       </div>
