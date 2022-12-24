9f622a8326dd2cf174c4a8f5e488924a566ef7f6
belkisarslan <belkisarslan33@gmail.com>
2022-12-24 13:47:01 +0300

    snow page added

--- src/counter/NewYear.js
+++ src/counter/NewYear.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import '../NewYear.css'
 
 const NewYear = () => {
   return (
     <div>
-      <h1>Hello 2023</h1>
+    <header>
+    <div className="snow snowfall">   
+    </div>
+    
+    <div className="snow2 snowfall2">
+    </div>
+    <div className="containerNewyear">
+    <div className="hat">
+        <div className="ball"></div>
+        <div className="body"></div>
+        <div className="base">.</div>
+        <p className="p">Happy New Year!</p>
+        <p className="p">Welcome 2023!</p>
+      </div>
+    </div>
+   </header>
     </div>
   )
 }
